a540f15c9bf42b0bfb15df211e92b0435366ab2d
ndtuan <ndtuanftd@gmail.com>
2022-12-04 18:10:50 +0700

    Add the draft for ploting feature

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,15 +1,23 @@
-import { React } from "react";
+import { React, useContext } from "react";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
+import PageTitle from "../../components/pageTitle/PageTitle";
 import "./dashboard.css";
 
+import AuthContext from "../../context/AuthContext";
+
 export const Dashboard = () => {
+  const authContext = useContext(AuthContext);
+  console.log("Authen context: " + authContext);
+  // console.log("Authen context of user: " + authContext.Users.);
   return (
     <div className="dashboard">
       <Sidebar />
       <div className="dashboardContainer">
         <Navbar />
-        <div className="content">Dashboard Container</div>
+        <div className="content">
+          {/* <PageTitle title={authContext.User} description={""}></PageTitle> */}
+        </div>
       </div>
     </div>
   );
--- frontend/src/pages/singleProject/singleProject.jsx
+++ frontend/src/pages/singleProject/singleProject.jsx
@@ -1,60 +1,154 @@
-import React, { useState } from 'react';
-import Sidebar from '../../components/sidebar/Sidebar';
-import Navbar from '../../components/navbar/Navbar';
-import PageTitle from '../../components/pageTitle/PageTitle';
-import { useParams, Navigate } from 'react-router-dom';
-import axios from 'axios';
-import { useEffect } from 'react';
-import { DataGrid } from '@mui/x-data-grid';
+import React, { useState } from "react";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import PageTitle from "../../components/pageTitle/PageTitle";
+import { useParams, Navigate } from "react-router-dom";
+import axios from "axios";
+import { useEffect } from "react";
+import { DataGrid } from "@mui/x-data-grid";
+// import CanvasJSReact from "../../assets/canvasjs.react";
+import { CanvasJSChart } from "canvasjs-react-charts";
+import "./singleProject.css";
 
 const SingleProject = () => {
   const { pk } = useParams();
-  const id = parseInt(pk); 
-  const [list, setList] = useState([])
-  const [notFound, setNotFound] = useState(false)
-  
+  const id = parseInt(pk);
+  const [list, setList] = useState([]);
+  const [notFound, setNotFound] = useState(false);
+  var followerPoints = [];
+  var followingPoints = [];
   useEffect(() => {
     axios({
       url: `http://localhost:8000/api/profile/?q=${id}`,
-      method: 'GET',
+      method: "GET",
     })
       .then((res) => {
         setList(() => res.data);
       })
+      .then((data) => {})
       .catch((err) => {
-        console.log(err)
-        setNotFound(true)
+        console.log(err);
+        setNotFound(true);
       });
   }, []);
 
-   const columns = [
-     { field: 'profile_id', headerName: 'Profile ID', width: 160 },
-     {field: 'name', headerName: 'Profile Name', width: 255},
-     {field: 'friend_count', headerName: 'Friends Count', width: 120},
-     {field: 'follower_count', headerName: 'Follower Count', width: 120},
-     {field: 'following_count', headerName: 'Following Count', width: 120},
-   ];
+  const columns = [
+    { field: "profile_id", headerName: "Profile ID", width: 160 },
+    { field: "name", headerName: "Profile Name", width: 255 },
+    { field: "friend_count", headerName: "Friends Count", width: 120 },
+    { field: "follower_count", headerName: "Follower Count", width: 120 },
+    { field: "following_count", headerName: "Following Count", width: 120 },
+  ];
 
-  if (id === null || isNaN(id) || notFound) return <Navigate to={'/error'} />
-  
+  if (id === null || isNaN(id) || notFound) return <Navigate to={"/error"} />;
+
+  /*
+   * Archive data for plotting
+   */
+  for (let index = 0; index < list.length; index++) {
+    followerPoints.push({
+      label: list[index].name,
+      y: list[index].follower_count,
+    });
+
+    followingPoints.push({
+      label: list[index].name,
+      y: list[index].following_count,
+    });
+  }
+  console.log("DataPoints", followerPoints);
+
+  var count = followerPoints.reduce((p, c) => {
+    var follower = c.y;
+    if (!p.hasOwnProperty(follower)) {
+      p[follower] = 0;
+    }
+    p[follower]++;
+    return p;
+  }, {});
+
+  console.log("count", count);
+  console.log("Count at 0", count[0]);
+
+  var countsExtended = Object.keys(count).map((k) => {
+    return { label: k, y: count[k] };
+  });
+  console.log("count extended", countsExtended);
+
+  const followerChartOption = {
+    animationEnable: true,
+    exportEnabled: true,
+    theme: "light1",
+    title: {
+      text: "Visualization for members's followers in group",
+    },
+    axisX: {
+      title: "Member",
+      reversed: true,
+    },
+    axisY: {
+      title: "Number of following",
+      includeZero: true,
+      // labelFormatter: addSymbols,
+    },
+    data: [
+      {
+        type: "column",
+        indexLabel: "{y}",
+        startAngle: -90,
+        dataPoints: countsExtended,
+      },
+    ],
+  };
+
+  /* Pie chart */
+  var pieChartData = [
+    { y: 20, label: "Airfare" },
+    { y: 24, label: "Food & Drinks" },
+    { y: 20, label: "Accomodation" },
+    { y: 14, label: "Transportation" },
+    { y: 12, label: "Activities" },
+    { y: 10, label: "Misc" },
+  ];
+  const pieOptions = {
+    animationEnabled: true,
+    exportEnabled: true,
+    theme: "light2", // "light1", "dark1", "dark2"
+    title: {
+      text: "Trip Expenses",
+    },
+    data: [
+      {
+        type: "pie",
+        indexLabel: "{label}: {y}%",
+        startAngle: -90,
+        dataPoints: pieChartData,
+      },
+    ],
+  };
+
   return (
-    <div className='projects'>
+    <div className="projects">
       <Sidebar />
-      <div className='projectsContainer'>
+      <div className="projectsContainer">
         <Navbar />
-        <div className='content'>
-          <PageTitle title={'Project Page'} description={`Project #${id}`} />
+        <div className="content">
+          <PageTitle title={"Project Page"} description={`Project #${id}`} />
           <div>
-            <DataGrid 
+            <DataGrid
               rows={list}
               columns={columns}
               pageSize={10}
               autoHeight={true}
               rowsPerPageOptions={[10]}
               getRowId={(rows) => {
-                return rows.profile_id ? rows.profile_id : 0
+                return rows.profile_id ? rows.profile_id : 0;
               }}
             />
+          </div>
+          <div className="Charts">
+            <CanvasJSChart options={followerChartOption} />
+            {/* <CanvasJSChart options={pieOptions} /> */}
           </div>
         </div>
       </div>
